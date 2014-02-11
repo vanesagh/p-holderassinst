495830cc3a4a53ce7062b26bc1e46b2ff8643f92
vanesagh <vanesag@chalmers.se>
2014-02-11 19:11:26 +0100

    finish static pages

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,7 @@
 module StaticPagesHelper
+ #template for repetitive titles
+  def wititle(wi_variant) #method definition
+  wi_title="P-Holder Assembly Instructions for "
+  "#{wi_title} #{wi_variant}" #string concatenation
+  end
 end
